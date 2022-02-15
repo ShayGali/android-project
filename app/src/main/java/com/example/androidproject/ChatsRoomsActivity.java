7842cf930b89e34e2e70eea92d61141f1fc3291e
Shay Gali <shaygali100@gmail.com>
2022-02-15 23:47:27 +0200

    הוספת כפתור לניווט בין החדרי צאטים לחדר של הצאט עצמו

--- app/src/main/java/com/example/androidproject/ChatsRoomsActivity.java
+++ app/src/main/java/com/example/androidproject/ChatsRoomsActivity.java
@@ -2,7 +2,9 @@ package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChatsRoomsActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ChatsRoomsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chats_rooms);
+    }
+
+    public void goToChat(View view) {
+        String roomID = "239ea67e-7b58-4c80-93ea-8f381f598f4f";
+        Intent intent = new Intent(this, ChatActivity.class);
+        intent.putExtra("roomID", roomID);
     }
 }
