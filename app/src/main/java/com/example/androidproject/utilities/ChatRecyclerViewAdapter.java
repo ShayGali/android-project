50accc6d05defb82d2375a137706313e384d0c03
Shay Gali <shaygali100@gmail.com>
2022-02-16 22:40:35 +0200

    סידור של ההודעות עדיין אי אפשר לשלוח

--- app/src/main/java/com/example/androidproject/utilities/ChatRecyclerViewAdapter.java
+++ app/src/main/java/com/example/androidproject/utilities/ChatRecyclerViewAdapter.java
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidproject.R;
 import com.example.androidproject.model.Message;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 
@@ -19,6 +20,9 @@ public class ChatRecyclerViewAdapter extends RecyclerView.Adapter<ChatRecyclerVi
 
     private ArrayList<Message> messages;
 
+    private static int VIEW_TYPE_SENT = 1;
+    private static int VIEW_TYPE_RECEIVED = 2;
+
     public ChatRecyclerViewAdapter(ArrayList<Message> messages) {
         this.messages = messages;
     }
@@ -26,17 +30,21 @@ public class ChatRecyclerViewAdapter extends RecyclerView.Adapter<ChatRecyclerVi
     @NonNull
     @Override
     public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.message_received_layout, parent, false);
-        MessageViewHolder messageViewHolder = new MessageViewHolder(view);
-        return messageViewHolder;
+        if(viewType == VIEW_TYPE_SENT)
+            return new MessageViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_send_layout, parent, false));
+
+        return new MessageViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.message_received_layout, parent, false));
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
         CardView cardView = holder.cardView;
         TextView messageContent = holder.messageContent;
+        TextView textTime = holder.textTime;
 
         messageContent.setText(messages.get(position).getMessageContent());
+        textTime.setText(messages.get(position).getTimestamp());
     }
 
     @Override
@@ -44,14 +52,29 @@ public class ChatRecyclerViewAdapter extends RecyclerView.Adapter<ChatRecyclerVi
         return messages.size();
     }
 
+    /**
+     *
+     * @param position the position of the message in the array
+     * @return if the message sent or received
+     */
+    @Override
+    public int getItemViewType(int position) {
+        if(messages.get(position).getSenderId().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+            return VIEW_TYPE_SENT;
+        }
+        return VIEW_TYPE_RECEIVED;
+    }
+
     public static class MessageViewHolder extends RecyclerView.ViewHolder {
         CardView cardView;
         TextView messageContent;
+        TextView textTime;
 
         public MessageViewHolder(@NonNull View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.card_view);
             messageContent = itemView.findViewById(R.id.message_content);
+            textTime = itemView.findViewById(R.id.text_time);
         }
     }
 }
